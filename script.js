832cd869fd67256e602a9ffbc1ecabe5b8c6ac97
Demo1website <vishwas4forjob@gmail.com>
2026-03-16 07:59:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 document.querySelector("form").addEventListener("submit",function(e){
 
 e.preventDefault()
 
-alert("Appointment request submitted!")
+alert("Appointment Request Submitted Successfully")
 
 })
